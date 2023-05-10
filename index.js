5bbfb71346b8a16c3ad6e2b2178ee50e2d4157c0
Abby <aheiser2@yahoo.com>
2023-05-10 16:30:48 -0500

    Add slideshow to side section, begin adding more pictures to it

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ const patterns = {
 
 }
 
+//Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+
+
+//Validation
 function validate(field, regex){
     // console.log(field)
    if(regex.test(field.value)){
