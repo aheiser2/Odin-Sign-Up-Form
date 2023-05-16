484d81685b2e4fc3f47484782744c5586730ddca
Abby <aheiser2@yahoo.com>
2023-05-15 21:15:16 -0500

    Finalize css, add alert to submit function

--- index.js
+++ index.js
@@ -17,18 +17,13 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 4 seconds
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
 
@@ -66,6 +61,7 @@ function submit(){
     inputs.forEach((input) => {
         input.value = ""
     })
+    alert("Thank you for your interest!")
 }
 
 submitButton.addEventListener('click', submit)
